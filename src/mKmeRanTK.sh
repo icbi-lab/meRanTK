a43590cf2d79c68b73e05aecb515de13787ebc75
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2022-06-13 17:03:56 +0200

    - remove meRanGt - remove test devel

--- src/mKmeRanTK.sh
+++ src/mKmeRanTK.sh
@@ -2,7 +2,6 @@
 
 
 /usr/local/bioinf/perl/perl-5.24.3/bin/pp -B -M Data::Dumper -M diagnostics -M GD -M GD::Text -M GD::Text::Align -M GD::Graph::lines -l /usr/lib64/libgd.so.2 -c -vv -o meRanGs meRanGs.pl
-/usr/local/bioinf/perl/perl-5.24.3/bin/pp -B -M Data::Dumper -M diagnostics -M GD -M GD::Text -M GD::Text::Align -M GD::Graph::lines -l /usr/lib64/libgd.so.2 -c -vv -o meRanGt meRanGt.pl
 /usr/local/bioinf/perl/perl-5.24.3/bin/pp -B -M Data::Dumper -M diagnostics -M GD -M GD::Text -M GD::Text::Align -M GD::Graph::lines -l /usr/lib64/libgd.so.2 -c -vv -o meRanGh meRanGh.pl
 /usr/local/bioinf/perl/perl-5.24.3/bin/pp -B -M Data::Dumper -M diagnostics -M GD -M GD::Text -M GD::Text::Align -M GD::Graph::lines -l /usr/lib64/libgd.so.2 -c -vv -o meRanT meRanT.pl
 /usr/local/bioinf/perl/perl-5.24.3/bin/pp -B -M Data::Dumper -l /lib64/libm.so.6 -c -vv -o meRanCall meRanCall.pl

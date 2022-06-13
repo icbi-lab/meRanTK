bc9df413d59d5028a6b76e6f05703b2be050773d
Dietmar Rieder <dietmar.rieder@i-med.ac.at>
2022-06-13 16:56:28 +0200

    update tests

--- util/runTests.sh
+++ util/runTests.sh
@@ -1,17 +1,17 @@
-    ./meRanT mkbsidx \
+    ./meRanT.pl mkbsidx \
       -t 2 \
       -fa ./testdata/refSeq/mm10.refSeqRNA-noPRED.500.fa \
       -id ./testdata/mm10/meRanTIDX/
 
-    ./meRanT align \
+    ./meRanT.pl align \
        -t 2 \
        -f ./testdata/fastq/clean_KHOD_400k_test.fastq \
        -i2g ./testdata/refSeq/mm10.refSeqRNA-noPRED.500.t2g.map \
        -o ./testdata/results \
        -S meRanT_test.sam  \
-       -x ./testdata/mm10/meRanTIDX/mm10.refSeqRNA-noPRED.500_C2T
+       -x ./testdata/mm10/meRanTIDX/mm10.refSeqRNA-noPRED.500_C2T -debug
 
-    ./meRanCall \
+    ./meRanCall.pl \
       -p 2 \
       -s ./testdata/results/meRanT_test.sam \
       -f ./testdata/refSeq/mm10.refSeqRNA-noPRED.500.fa \
@@ -22,12 +22,12 @@
       -mBQ 30 \
       -sc 5
 
-   ./meRanT mkbsidx \
+   ./meRanT.pl mkbsidx \
       -t 2 \
       -fa ./testdata/tRNAs/mm10.tRNAs.20140204.fa \
       -id ./testdata/mm10/meRanT_tRNA_IDX
 
-    ./meRanT align \
+    ./meRanT.pl align \
        -t 2 \
        -f ./testdata/fastq/clean_KHOD_400k_test.fastq \
        -i2g ./testdata/tRNAs/mm10.tRNA.20140204.map \
@@ -35,7 +35,7 @@
        -S meRanT_tRNAs_test.sam  \
        -x ./testdata/mm10/meRanT_tRNA_IDX/mm10.tRNAs.20140204_C2T
 
-   ./meRanCall \
+   ./meRanCall.pl \
       -p 2 \
       -s ./testdata/results/meRanT_tRNAs_test.sam \
       -f ./testdata/tRNAs/mm10.tRNAs.20140204.fa \
@@ -46,8 +46,7 @@
       -mBQ 30 \
       -sc 5
 
-
-   ./meRanGs mkbsidx  \
+   ./meRanGs.pl mkbsidx  \
       -t 2 \
       -fa ./testdata/mm10/chr19.fa \
       -GTF ./testdata/mm10/ref_GRCm38.p2_top_level_no_prediction_chr19_sort.gff3 \
@@ -55,7 +54,7 @@
       -GTFtagEPG gene \
       -id ./testdata/mm10/meRanGsIDX
 
-    ./meRanGs align \
+    ./meRanGs.pl align \
       -t 2 \
       -f ./testdata/fastq/clean_KHOD_400k_test.fastq \
       -id ./testdata/mm10/meRanGsIDX \
@@ -66,7 +65,7 @@
       -un \
       --star_genomeLoad NoSharedMemory
 
-    ./meRanCall \
+    ./meRanCall.pl \
       -p 2 \
       -s ./testdata/results/meRanGs_test_sorted.bam \
       -f ./testdata/mm10/chr19.fa \
@@ -78,13 +77,49 @@
       -bed63 \
       -sc 5
 
+    ./meRanGs.pl align \
+      -t 2 \
+      -f ./testdata/fastq/r1.fastq \
+      -r ./testdata/fastq/r2.fastq \
+      -id ./testdata/mm10/meRanGsIDX \
+      -bg \
+      -o ./testdata/results \
+      -S meRanGs_test_pe.sam \
+      -MM \
+      -un \
+      --star_genomeLoad NoSharedMemory \
+      -mbp -deleteSAM -dt -star_outFilterScoreMinOverLread 0.40
 
-    ./meRanGh mkbsidx  \
+
+    ./meRanCall.pl \
+      -p 12 \
+      -s ./testdata/results/meRanGs_test_pe_sorted.bam \
+      -f ./testdata/mm10/chr19.fa \
+      -gref \
+      -o ./testdata/results/meRanGs_pe_meRanCall_m5Cs.txt \
+      -gtf ./testdata/mm10/gencode_mm10_chr19.gtf \
+      -C_cutoff 5 \
+      -mcov 5 \
+      -mc 1 \
+      -mr 0.1 \
+      -ei 0.1 \
+      -sc 10 \
+      -fdr 0.05 \
+      -np \
+      -gcr \
+      -rl 150 \
+      -md 5 \
+      -cr 0.99 \
+      -mBQ 30 \
+      -bed63 \
+      -sc 5
+
+    ./meRanGh.pl mkbsidx  \
       -t 2 \
       -fa ./testdata/mm10/chr19.fa \
       -id ./testdata/mm10/meRanGhIDX
 
-    ./meRanGh align \
+    ./meRanGh.pl align \
       -t 2 \
       -f ./testdata/fastq/clean_KHOD_400k_test.fastq \
       -id ./testdata/mm10/meRanGhIDX \
@@ -94,16 +129,62 @@
       -MM \
       -un
 
-    ./meRanCall \
+    ./meRanCall.pl \
       -p 2 \
       -s ./testdata/results/meRanGh_test_sorted.bam \
       -f ./testdata/mm10/chr19.fa \
       -gref \
       -o ./testdata/results/meRanGh_meRanCall_m5Cs.txt \
+      -gtf ./testdata/mm10/gencode_mm10_chr19.gtf \
+      -C_cutoff 5 \
+      -mcov 5 \
+      -mc 1 \
+      -mr 0.1 \
+      -ei 0.1 \
+      -sc 10 \
+      -fdr 0.05 \
+      -np \
+      -gcr \
+      -rl 150 \
       -md 5 \
       -cr 0.99 \
       -mBQ 30 \
       -bed63 \
       -sc 5
 
+    ./meRanGh.pl align \
+      -t 2 \
+      -f ./testdata/fastq/r1.fastq \
+      -r ./testdata/fastq/r2.fastq \
+      -id ./testdata/mm10/meRanGhIDX \
+      -bg \
+      -o ./testdata/results \
+      -S meRanGh_test_pe.sam \
+      -mbp -deleteSAM \
+      -dt \
+      -MM \
+      -un \
+      -dovetail \
 
+    ./meRanCall.pl \
+      -p 12 \
+      -s ./testdata/results/meRanGh_test_pe_sorted.bam \
+      -f ./testdata/mm10/chr19.fa \
+      -gref \
+      -o ./testdata/results/meRanGh_pe_meRanCall_m5Cs.txt \
+      -gtf ./testdata/mm10/gencode_mm10_chr19.gtf \
+      -C_cutoff 5 \
+      -mcov 5 \
+      -mc 1 \
+      -mr 0.1 \
+      -ei 0.1 \
+      -sc 10 \
+      -fdr 0.05 \
+      -np \
+      -gcr \
+      -rl 150 \
+      -md 5 \
+      -cr 0.99 \
+      -mBQ 30 \
+      -bed63 \
+      -sc 5
